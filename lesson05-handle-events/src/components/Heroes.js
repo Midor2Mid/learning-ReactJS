13930239c6ba999f78047ea0e91539aa48eb5c9f
Midor2Mid <14520777@gm.uit.edu.vn>
2019-04-19 17:44:49 +0700

    Cách 2 handle events: dùng arrow function

--- lesson05-handle-events/src/components/Heroes.js
+++ lesson05-handle-events/src/components/Heroes.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 class Heroes extends Component {
 
-    constructor(props){
-        super(props);
-        this.onAddToCart=this.onAddToCart.bind(this);
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.onAddToCart=this.onAddToCart.bind(this);
+    // }
 
-    onAddToCart() {
+    // onAddToCart() {
+    //     alert(this.props.children + ' - ' + this.props.type)
+    // }
+    onAddToCart2 = () => {
         alert(this.props.children + ' - ' + this.props.type)
     }
     render() {
@@ -24,7 +27,7 @@ class Heroes extends Component {
                             </p>
                             <p>
                                 {/* <a href="/" className="btn btn-primary" onClick={() => { this.onAddToCart(this.props.children) }}>Pick</a> */}
-                                <a href="/" className="btn btn-primary" onClick={ this.onAddToCart }>Pick</a> 
+                                <a href="/" className="btn btn-primary" onClick={ this.onAddToCart2 }>Pick</a> 
                             </p>
                         </div>
                     </div>
