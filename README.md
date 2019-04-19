e1c6ba6a0f4e5c0d3f0363c59eb4efdacbe876ab
Midor2Mid <14520777@gm.uit.edu.vn>
2019-04-19 17:42:22 +0700

    Cách 1 handle events: dùng constructor

--- README.md
+++ README.md
@@ -71,4 +71,8 @@
 # Tuần 6: Handle Events - Xử lí sự kiện
 * Copy source code từ tuần 5, trong app.js tạo thêm 1 button, thêm thuộc tính cho button: `onClick={this.onClick}` //Đây là trong class nên cần gọi `this.<tên function>` (ở đây là onClick).
   *  Lưu ý function onClick phải đưa ra ngoài render() nhưng vẫn ở bên trong class.
-  *  Danh sách [events](https://reactjs.org/docs/events.html)
+  *  Danh sách [events](https://reactjs.org/docs/events.html) mà ReactJS hỗ trợ. (onClick, onDoubleClick,...).
+* Để gọi và sử dụng props (thuộc tính) có thể dùng:
+  *  Tạo constructor có tham số props và gọi super(props)
+  *   Để truy cập this tại function: this.tên_function = this.tên_function.bind(this)
+  *  Tên_function = () => {}
--- lesson05-handle-events/src/components/Heroes.js
+++ lesson05-handle-events/src/components/Heroes.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 
 class Heroes extends Component {
+
+    constructor(props){
+        super(props);
+        this.onAddToCart=this.onAddToCart.bind(this);
+    }
+
+    onAddToCart() {
+        alert(this.props.children + ' - ' + this.props.type)
+    }
     render() {
         return (
             <h1>
-                
+
                 <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                     <div className="thumbnail">
-                        <img alt={this.props.children} src={this.props.image}/>
+                        <img alt={this.props.children} src={this.props.image} />
                         <div className="caption">
                             <h3>{this.props.children}</h3>
                             <p>
                                 {this.props.type}
                             </p>
                             <p>
-                                <a href="/" className="btn btn-primary">Pick</a>
+                                {/* <a href="/" className="btn btn-primary" onClick={() => { this.onAddToCart(this.props.children) }}>Pick</a> */}
+                                <a href="/" className="btn btn-primary" onClick={ this.onAddToCart }>Pick</a> 
                             </p>
                         </div>
                     </div>
                 </div>
-                
+
             </h1>
         );
     }
